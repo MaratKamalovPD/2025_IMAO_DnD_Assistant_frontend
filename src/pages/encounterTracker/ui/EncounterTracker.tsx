f362a0ffd2c0d5e17c3f5a27ed182d1889471b59
AlexeiLDD <doroga218@gmail.com>
2025-03-15 04:32:45 +0300

    Added CardList ui component, adde it to encounter tracker

--- src/pages/encounterTracker/ui/EncounterTracker.tsx
+++ src/pages/encounterTracker/ui/EncounterTracker.tsx
@@ -1,7 +1,17 @@
+import { mockCreatures } from 'entities/creature/lib';
+import { creatureActions } from 'entities/creature/model';
+import { useDispatch } from 'react-redux';
+import { CardList } from './cardList';
 import { TrackPanel } from './trackPanel';
 
-export const EncounterTracker = () => (
-  <div className='battle-layout'>
-    <TrackPanel />
-  </div>
-);
+export const EncounterTracker = () => {
+  const dispatch = useDispatch();
+  dispatch(creatureActions.addCreatures(mockCreatures));
+
+  return (
+    <div className='battle-layout'>
+      <TrackPanel />
+      <CardList />
+    </div>
+  );
+};
--- src/pages/encounterTracker/ui/trackPanel/TrackPanel.tsx
+++ src/pages/encounterTracker/ui/trackPanel/TrackPanel.tsx
@@ -1,9 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 
 import {
-  EncounterStore,
+  Creature,
+  creatureSelectors,
+  CreaturesStore,
+} from 'entities/creature/model';
+import {
+  encounterActions,
   EncounterState,
-  nextTurn,
+  EncounterStore,
 } from 'entities/encounter/model';
 
 export const TrackPanel = () => {
@@ -12,11 +17,17 @@ export const TrackPanel = () => {
   const { currentRound, currentTurnIndex, participants } =
     useSelector<EncounterStore>((state) => state.encounter) as EncounterState;
 
+  const { name } = useSelector<CreaturesStore>((state) =>
+    creatureSelectors.selectById(state, participants[currentTurnIndex]),
+  ) as Creature;
+
   return (
     <div className='tracker'>
       <span>Раунд: {currentRound}</span>
-      <span>Ход: {participants[currentTurnIndex]}</span>
-      <button onClick={() => dispatch(nextTurn())}>Следующий ход</button>
+      <span>Ход: {name}</span>
+      <button onClick={() => dispatch(encounterActions.nextTurn())}>
+        Следующий ход
+      </button>
     </div>
   );
 };
