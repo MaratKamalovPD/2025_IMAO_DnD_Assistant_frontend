dfb67f29073eea36d333f8c768ed1fea57401452
AlexeiLDD <doroga218@gmail.com>
2025-03-24 04:10:04 +0300

    Added Feature and Hero components

---
+++ src/pages/main/ui/hero/Hero.tsx
@@ -0,0 +1,24 @@
+import s from './Hero.module.scss';
+
+import dndLogo from 'shared/assets/images/DnD_logo.png';
+import logo from 'shared/assets/images/logo.png';
+
+export const Hero = () => {
+  return (
+    <section className={s.hero}>
+      <div>
+        <img className={s.logo} src={logo} alt='Encounterium'></img>
+      </div>
+      <ul className={s.styledList}>
+        <li>Автоматизируйте свои игры</li>
+        <li>Управляйте бестиарием</li>
+        <li>Создавайте сражения</li>
+        <li>Погружайтесь в мир</li>
+      </ul>
+      <div>
+        <img className={s.dndLogo} src={dndLogo} alt='DnD_logo'></img>
+      </div>
+      <button data-variant='primary'>Начать использовать</button>
+    </section>
+  );
+};
