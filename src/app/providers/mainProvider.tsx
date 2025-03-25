da62aafb57ae45a6c4509e9e6670e5fa56a0e43c
AlexeiLDD <99890687+AlexeiLDD@users.noreply.github.com>
2025-03-25 22:41:42 +0300

    Feature/header (#18)
    * added header and header providor
    
    * Added scroling header and mobile fixes

--- src/app/providers/mainProvider.tsx
+++ src/app/providers/mainProvider.tsx
@@ -1,11 +1,7 @@
-import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 
 import { store } from 'app/store';
-
-type Props = {
-  children: ReactNode;
-};
+import { Props } from './types';
 
 export const MainProviders = ({ children }: Props) => {
   return <Provider store={store}>{children}</Provider>;
