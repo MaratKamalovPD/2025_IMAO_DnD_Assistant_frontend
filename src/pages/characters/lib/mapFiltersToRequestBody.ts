eb82df00f018607d8e4d4cef13f77162a2a84740
IlyaChgn <149610530+IlyaChgn@users.noreply.github.com>
2025-05-06 21:23:21 +0300

    Fix/multiple minor (#51)
    * fixed sort in bestiary (probably)
    
    * fixed characters search

--- src/pages/characters/lib/mapFiltersToRequestBody.ts
+++ src/pages/characters/lib/mapFiltersToRequestBody.ts
@@ -1,59 +1,18 @@
-import { GetCreaturesRequest } from 'pages/bestiary/api';
-import { Filters, OrderParams } from 'pages/bestiary/model';
-
-const filterMapping: { [key: string]: keyof GetCreaturesRequest['filter'] } = {
-  'Иммунитет к урону': 'immunityDamage',
-  'Иммунитет к состояниям': 'immunityCondition',
-  'Сопротивление к урону': 'resistanceDamage',
-  'Уязвимость к урону': 'vulnerabilityDamage',
-  Чувства: 'senses',
-  Перемещение: 'moving',
-  'Размер существа': 'size',
-  'Тип существа': 'type',
-  'Класс опасности': 'challengeRating',
-  'Места обитания': 'environment',
-  Умения: 'features',
-};
+import { GetCharactersRequest } from 'pages/characters/api';
 
 export const mapFiltersToRequestBody = (
-  filters: Filters,
   start: number,
   size: number,
   searchValue: string,
-  orderParams: OrderParams[],
-): GetCreaturesRequest => {
-  const requestBody: GetCreaturesRequest = {
+): GetCharactersRequest => {
+  const requestBody: GetCharactersRequest = {
     start: start,
     size: size,
     search: {
       value: searchValue,
       exact: false,
     },
-    order: orderParams,
-    filter: {
-      book: [], // фильтр по книгам
-      npc: [], // фильтр по NPC
-      challengeRating: [], // фильтр по уровню сложности
-      type: [], // фильтр по типу существа
-      size: [], // фильтр по размеру
-      tag: [], // фильтр по тегам
-      moving: [], // фильтр по способу перемещения
-      senses: [], // фильтр по чувствам
-      vulnerabilityDamage: [], // фильтр по уязвимостям к урону
-      resistanceDamage: [], // фильтр по сопротивлению к урону
-      immunityDamage: [], // фильтр по иммунитету к урону
-      immunityCondition: [], // фильтр по иммунитету к состояниям
-      features: [], // фильтр по особенностям
-      environment: [], // фильтр по окружению
-    },
   };
-
-  Object.entries(filters).forEach(([key, values]) => {
-    const mappedKey = filterMapping[key];
-    if (mappedKey && requestBody.filter[mappedKey]) {
-      requestBody.filter[mappedKey] = values;
-    }
-  });
 
   return requestBody;
 };
